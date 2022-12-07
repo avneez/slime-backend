0c1af82fe2e8a3f3588a502ba0c3cbe3da545033
Avneez Bhargkeshari <61105948+avneez@users.noreply.github.com>
2022-12-07 13:53:15 +0530

    backend slime

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv'
+
+import postRoutes from './routes/posts.js'
+import userRoutes from './routes/users.js'
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes)
+app.use('/user', userRoutes)
+
+app.get('/', (req, res)=>{
+  res.send('Hello to Slime App');
+});
+
+const PORT = process.env.PORT || 5000;
+
+try {
+  await mongoose.connect(process.env.CONNECTION_URL);
+} catch (error) {
+  console.log('connnection error', error)
+}
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
